a893339566bec7b3214d247bf1a392c394e1a347
nizomiddin-rakhimberdiev <nizomiddinoff@mail.ru>
2025-11-27 10:26:27 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+from aiogram import Bot, Dispatcher, types, F
+from fastapi import FastAPI, Request
+from aiogram.types import Update
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler
+
+import asyncio
+TOKEN = "8439567445:AAHnQ3OK-27cLIz5xTrUlM6_gzZlESVUhdw"
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+WEBHOOK_URL = f"https://mybot.onrender.com/webhook"
+app = FastAPI()
+
+types.ContentType
+
+@dp.message(F.text=='/start')
+async def start(message: types.Message):
+    await message.answer(text="Assalamu alaykum bratiiiim!")
+
+
+@dp.message(F.text=='/location')
+async def photo_handler(message: types.Message):
+    latitude = 44.287382516166296
+    longitude = 65.2055360005088
+    await message.answer_location(latitude=latitude, longitude=longitude)
+
+@dp.message(F.text=='/contact')
+async def photo_handler(message: types.Message):
+    await message.answer_contact(phone_number='+998994786207', first_name="Hech kimsan")
+
+
+@dp.message(F.voice)
+async def voice_handler(message: types.Message):
+    print(message.voice)
+    await message.answer_voice(voice=message.voice.file_id)
+
+@dp.message(F.contact)
+async def contact_handler(message: types.Message):
+    print(message.contact)
+    await message.answer_contact(phone_number=message.contact.phone_number, first_name=message.contact.first_name)
+
+@app.post(WEBHOOK_PATH)
+async def webhook(request: Request):
+    data = await request.json()
+    update = Update(**data)
+    await dp.feed_update(bot, update)
+    return {"ok": True}
+
+@app.on_event("startup")
+async def on_startup():
+    await bot.set_webhook(WEBHOOK_URL)
+
+async def main():
+    print("Bot ishga tushdi...")
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
